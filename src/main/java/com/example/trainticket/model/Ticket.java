4982b5b872b6acc74cb806f1d53f6ef3890765a6
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-09-05 18:33:41 +0530

    commit

--- src/main/java/com/example/trainticket/model/Ticket.java
+++ src/main/java/com/example/trainticket/model/Ticket.java
@@ -1,10 +1,12 @@
 package com.example.trainticket.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Ticket {
 
     @Id
