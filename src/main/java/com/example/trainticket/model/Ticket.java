fc396371f805f36855fff6136b72882cad648523
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-09-05 18:15:02 +0530

    model Ticket

--- src/main/java/com/example/trainticket/model/Ticket.java
+++ src/main/java/com/example/trainticket/model/Ticket.java
@@ -1,5 +1,22 @@
 package com.example.trainticket.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Ticket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fromLocation;
+    private String toLocation;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private double pricePaid;
+    private String section;
+    private int seatNumber;
 }
